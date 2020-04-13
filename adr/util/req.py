33c6d0da885e5784cf4dce4bedbdc4054e3f6e63
Kyle Lahnakoski <kyle@lahnakoski.com>
2020-04-13 15:34:52 -0400

    use defaults

--- adr/util/req.py
+++ adr/util/req.py
@@ -4,12 +4,16 @@ from requests.packages.urllib3.util.retry import Retry
 
 from adr.util import memoize
 
+DEFAULT_RETRIES = 3
+DEFAULT_BACKOFF_FACTOR = 0.3
+DEFAULT_STATUS_FORCELIST = (500, 502, 504)
+
 
 @memoize
 def requests_retry_session(
-    retries=3,
-    backoff_factor=0.3,
-    status_forcelist=(500, 502, 504),
+    retries=DEFAULT_RETRIES,
+    backoff_factor=DEFAULT_BACKOFF_FACTOR,
+    status_forcelist=DEFAULT_STATUS_FORCELIST,
     session=None,
 ):
     session = session or requests.Session()
@@ -21,6 +25,6 @@ def requests_retry_session(
         status_forcelist=status_forcelist,
     )
     adapter = HTTPAdapter(max_retries=retry)
-    session.mount('http://', adapter)
-    session.mount('https://', adapter)
+    session.mount("http://", adapter)
+    session.mount("https://", adapter)
     return session
